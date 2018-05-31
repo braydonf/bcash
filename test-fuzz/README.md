b8dc748b34cd0b19d01b08afa4d91477f3e0eaa3
Braydon Fuller <courier@braydon.com>
2018-05-30 20:04:45 -0700

    test: start bitcoinconsensus and bcoin script fuzz programs

--- test-fuzz/README.md
+++ test-fuzz/README.md
@@ -2,6 +2,28 @@
 
 This is a collection of scripts and tests for running fuzz testing Script against multiple implementations.
 
+## Build
+
+Clone, build and install libbitcoinconsensus shared library:
+```
+git clone https://github.com/Bitcoin-ABC/bitcoin-abc.git
+git checkout v0.17.1
+cd bitcoin-abc
+./autogen.sh
+./configure --prefix=/path/to/build --disable-wallet --with-gui=no --disable-zmq --disable-bench --disable-tests --disable-gui-tests --with-miniupnpc=no
+make -j 8
+make install
+```
+
+We've used the `/path/to/build` as the prefix as we're likely going to have multiple builds that may conflict.
+
+
+Build fuzz programs:
+```
+export CXXFLAGS="-I/path/to/build/include -L/path/to/build/lib"
+make
+```
+
 ## Notes
 
 Fuzz testing against Node.js bindings to [libbitcoinconsensus](https://github.com/Bitcoin-ABC/bitcoin-abc/blob/master/doc/shared-libraries.md) is fragile, as the build process is dependent upon the build system for configuration in way that makes working with gyp build process crufty and error prone. So instead we have a simple C program linked to against libbitcoinconsensus that will take input from a file, similarly we have an identical version for bcash implementation. The results of the implementations can be compared by giving the same data to each program to execute. This should also provide compatability with tools such as [AFL](http://lcamtuf.coredump.cx/afl/).
---
+++ test-fuzz/verify.cc
@@ -0,0 +1,90 @@
+/*!
+ * verify.cc - verify script for fuzz testing
+ * Copyright (c) 2018, the bcoin developers (MIT License).
+ * Parts of this are based on https://github.com/jonasnick/bitcoinconsensus_testcases
+ * https://github.com/bcoin-org/bcoin
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <errno.h>
+#include <bitcoinconsensus.h>
+
+#define LINE_MAX_HEX_LENGTH 20000
+#define LINE_MAX_LENGTH 10000
+
+int read_hex_line(const char *buf, int bufLen, unsigned char out[], int n)
+{
+    int i = 0;
+    int j = 0;
+
+    while (buf[i] != '\n' && buf[i] != '\0' && j < n && i + 1 < bufLen) {
+        char b;
+        sscanf(&buf[i], "%2hhx", &b);
+        out[j] = b;
+        i += 2;
+        j += 1;
+    }
+
+    return j;
+}
+
+int read_file(const char *filepath,
+              unsigned char scriptPubKey[], unsigned int *scriptPubKeyLen,
+              unsigned char txTo[], unsigned int *txToLen,
+              unsigned int *nIn, unsigned int *flags)
+{
+    FILE *fp = fopen(filepath, "r");
+    if (fp == NULL)
+        return ENOENT;
+
+    char line[LINE_MAX_HEX_LENGTH];
+
+    // first line is scriptPubKey (output script)
+    if (fgets(line, LINE_MAX_HEX_LENGTH, fp) != NULL)
+        *scriptPubKeyLen = read_hex_line(line, LINE_MAX_HEX_LENGTH, scriptPubKey, LINE_MAX_LENGTH);
+
+    // second line is txTo (tx)
+    if (fgets(line, LINE_MAX_HEX_LENGTH, fp) != NULL)
+        *txToLen = read_hex_line(line, LINE_MAX_HEX_LENGTH, txTo, LINE_MAX_LENGTH);
+
+    // third line is the nIn (input number)
+    if (fgets(line, LINE_MAX_HEX_LENGTH, fp) != NULL)
+        *nIn = (unsigned int)atoi(line);
+
+    // fourth line are the script flags
+    if (fgets(line, LINE_MAX_HEX_LENGTH, fp) != NULL)
+        *flags = (unsigned int)atoi(line);
+
+    fclose(fp);
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    uint8_t scriptPubKey[LINE_MAX_HEX_LENGTH];
+    unsigned int scriptPubKeyLen;
+    uint8_t txTo[LINE_MAX_HEX_LENGTH];
+    unsigned int txToLen;
+    unsigned int nIn;
+    unsigned int flags;
+
+    int status = 0;
+    const char *filepath = argv[1];
+    status = read_file(filepath, scriptPubKey, &scriptPubKeyLen, txTo,
+                       &txToLen, &nIn, &flags);
+
+    if (status)
+        return status;
+
+    bitcoinconsensus_error err = bitcoinconsensus_ERR_OK;
+    status = bitcoinconsensus_verify_script(scriptPubKey, scriptPubKeyLen,
+                                            txTo, txToLen, nIn, flags, &err);
+
+    printf("%d\n", status);
+
+    return status;
+}
